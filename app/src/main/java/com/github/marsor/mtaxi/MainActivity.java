6fa9b81bec781cea241051ac54b05e3a95434b58
marsor <369135912@qq.com>
2017-10-13 23:03:59 +0800

    Initial commit

---
+++ app/src/main/java/com/github/marsor/mtaxi/MainActivity.java
@@ -0,0 +1,22 @@
+package com.github.marsor.mtaxi;
+
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            final AnimatedVectorDrawable anim1 = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.anim);
+            final ImageView logo = (ImageView) findViewById(R.id.logo);
+            logo.setImageDrawable(anim1);
+            anim1.start();
+        }
+    }
+}
